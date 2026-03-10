51ba891777571841f27f811d21b056ed436e7783
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-03-10 22:28:50 +0100

    menu and other UI adjustments

--- components/ourMenu.tsx
+++ components/ourMenu.tsx
@@ -1,53 +1,38 @@
 "use client";
 
-import { MapPinLine, CurrencyNgn } from "@phosphor-icons/react";
+import { CurrencyNgn } from "@phosphor-icons/react";
 import { useRef } from "react";
 import Image from "next/image";
 
-interface houseType {
+interface foodType {
   image: string;
   name: string;
-  location: string;
   price: string;
-  oldPrice: string;
-  buttonText: string;
 }
 
-const houses: houseType[] = [
+const menuItems: foodType[] = [
   {
-    image: "/houseType1-1.png",
-    name: "Royal Mansions",
-    location: "Centenary city Airport road, Abuja",
-    price: "7million",
-    oldPrice: "9.5million",
-    buttonText: "More details",
+    image: "/hero-image.png",
+    name: "Burger",
+    price: "1000",
   },
 
   {
-    image: "/houseType1-2.png",
-    name: "4 bedroom terrace duplex",
-    location: "Wuye, Abuja",
-    price: "10million",
-    oldPrice: "12.5million",
-    buttonText: "More details",
+    image: "/xPizza 2.png",
+    name: "Pizza",
+    price: "2500",
   },
 
   {
-    image: "/houseType1-3.png",
-    name: "The Chateaux",
-    location: "Life camp, beside paradise estate, Abuja",
-    price: "7.5million",
-    oldPrice: "8million",
-    buttonText: "More details",
+    image: "/xShawarma 2.png",
+    name: "Shawarma",
+    price: "3000",
   },
 
   {
-    image: "/houseType1.png",
-    name: "7 bedroom villa",
-    location: "Cosgroove smart estate Guzape, Abuja",
-    price: "12million",
-    oldPrice: "14million",
-    buttonText: "More details",
+    image: "/hero-image.png",
+    name: "Burger",
+    price: "1500",
   },
 ];
 
@@ -64,46 +49,28 @@ export default function OurMenu() {
 
       <div
         ref={sectionRef}
-        className="w-full flex flex-col md:flex-row md:items-center gap-4 px-6 md:px-12"
+        className="w-full flex flex-col md:flex-row md:items-center justify-center gap-0 px-6 md:px-12 my-12"
       >
-        {houses.map((house, index) => (
+        {menuItems.map((item, index) => (
           <div
             ref={(el) => {
               if (el) cardsRef.current[index] = el;
             }}
-            key={house.name}
-            className="house-card w-full border-[1px] border-[#EEE9E6] rounded-[8px] property-card"
+            key={index}
+            className="house-card bg-[#fff] flex flex-col items-center w-60 border-[1px] border-[#EEE9E6] rounded-[0px]"
           >
-            <div className="relative h-70 md:h-70 lg:h-80 rounded-[8px]">
-              <Image
-                src={house.image}
-                fill
-                alt="house image"
-                className="rounded-[8px]"
-              />
+            <div className="relative w-full h-50 md:h-60 lg:h-60">
+              <Image src={item.image} fill alt="house image" className="" />
             </div>
             <div className="px-2 py-4 space-y-2">
-              <h3 className="text-[1.125rem] font-medium">{house.name}</h3>
-              <div className="flex items-center gap-1">
-                <MapPinLine size={20} />
-                <p className="text-[#737373] line-clamp-1">{house.location}</p>
-              </div>
+              <h3 className="text-[1.125rem] font-medium">{item.name}</h3>
 
               <div className="flex item-center gap-3 text-[1.125rem] font-medium">
                 <div className="flex items-center">
                   <CurrencyNgn size={20} />
-                  <span className="line-through">{house.oldPrice}</span>
-                </div>
-
-                <div className="flex items-center">
-                  <CurrencyNgn size={20} />
-                  <p> {house.price}</p>
+                  <p> {Number(item.price).toLocaleString("en-NG")}</p>
                 </div>
               </div>
-
-              <button className="inline-block bg-[#BF6028] text-[#f3f3f3] px-4 py-3 rounded-[8px] mt-3">
-                {house.buttonText}
-              </button>
             </div>
           </div>
         ))}
