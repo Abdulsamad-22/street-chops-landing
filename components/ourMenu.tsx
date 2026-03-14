5f9575b6f5ee10cd3a86411bd16844e7d9991b88
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-03-14 17:52:28 +0100

    much better images used to bring out beauty of this section

--- components/ourMenu.tsx
+++ components/ourMenu.tsx
@@ -18,7 +18,7 @@ const menuItems: foodType[] = [
   },
 
   {
-    image: "/xPizza 2.png",
+    image: "/pizzaBg.png",
     name: "Pizza",
     price: "2500",
   },
@@ -30,8 +30,8 @@ const menuItems: foodType[] = [
   },
 
   {
-    image: "/hero-image.png",
-    name: "Burger",
+    image: "/plainBgChips.png",
+    name: "Chips & Fries",
     price: "1500",
   },
 ];
@@ -39,9 +39,13 @@ const menuItems: foodType[] = [
 export default function OurMenu() {
   const sectionRef = useRef<HTMLDivElement | null>(null);
   const cardsRef = useRef<HTMLDivElement[]>([]);
+
   return (
-    <section>
-      <div className="text-center">
+    <section className="menu-section mb-20">
+      <div className="text-center mb-8">
+        <div className="text-center text-[1.125rem] md:text-[1.5rem] font-bold">
+          Our Menu
+        </div>
         <h2 className="text-[3rem] font-semibold">
           Savour the Journey, Enjoy the Delight
         </h2>
@@ -49,7 +53,7 @@ export default function OurMenu() {
 
       <div
         ref={sectionRef}
-        className="w-full flex flex-col md:flex-row md:items-center justify-center gap-0 px-6 md:px-12 my-12"
+        className="w-full flex flex-col md:flex-row md:items-center justify-center gap-0 px-6 md:px-12 mb-12"
       >
         {menuItems.map((item, index) => (
           <div
@@ -57,19 +61,17 @@ export default function OurMenu() {
               if (el) cardsRef.current[index] = el;
             }}
             key={index}
-            className="house-card bg-[#fff] flex flex-col items-center w-60 border-[1px] border-[#EEE9E6] rounded-[0px]"
+            className="menu-card bg-[#fff] hover:bg-[#b70e10] hover:text-[#f3f3f3] transition-colors transform-0.5 flex flex-col w-60 border-[1px] border-[#EEE9E6] rounded-[0px]"
           >
-            <div className="relative w-full h-50 md:h-60 lg:h-60">
+            <div className="relative w-full h-50 md:h-60 lg:h-55">
               <Image src={item.image} fill alt="house image" className="" />
             </div>
             <div className="px-2 py-4 space-y-2">
               <h3 className="text-[1.125rem] font-medium">{item.name}</h3>
 
-              <div className="flex item-center gap-3 text-[1.125rem] font-medium">
-                <div className="flex items-center">
-                  <CurrencyNgn size={20} />
-                  <p> {Number(item.price).toLocaleString("en-NG")}</p>
-                </div>
+              <div className="flex items-center">
+                <CurrencyNgn size={20} />
+                <p> {Number(item.price).toLocaleString("en-NG")}</p>
               </div>
             </div>
           </div>
